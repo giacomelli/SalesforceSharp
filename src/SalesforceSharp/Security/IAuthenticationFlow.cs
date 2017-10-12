ef0c79eb7fc41e9c6e1a1cf64583a17c7c9e2c7f
John Deese <john.deese@blackbaud.com>
2017-10-12 12:48:36 -0400

    Added Async functions using the async/await pattern

--- src/SalesforceSharp/Security/IAuthenticationFlow.cs
+++ src/SalesforceSharp/Security/IAuthenticationFlow.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Threading;
 using System.Threading.Tasks;
 
 namespace SalesforceSharp.Security
@@ -20,10 +17,10 @@ namespace SalesforceSharp.Security
     ///         ◊ Web server flow, where the server can securely protect the consumer secret.
     ///         ◊ User-agent flow, used by applications that cannot securely store the consumer secret.
     ///         ◊ Username-password flow, where the application has direct access to user credentials.
-    ///         
+    ///
     /// After successfully authenticating the user, you’ll receive an access token which can be used to make authenticated REST API calls.
-    /// 
-    /// More info at: 
+    ///
+    /// More info at:
     /// Digging Deeper into OAuth 2.0 on Force.com: http://wiki.developerforce.com/page/Digging_Deeper_into_OAuth_2.0_on_Force.com
     /// Force.com REST API Developer's Guide: docs/Force.com REST API Developer's Guide.pdf
     /// </remarks>
@@ -38,5 +35,14 @@ namespace SalesforceSharp.Security
         /// </remarks>
         /// <returns>The authentication info with access token and instance url for futher API calls.</returns>
         AuthenticationInfo Authenticate();
+
+        /// <summary>
+        /// Authenticate in the Salesforce REST's API.
+        /// </summary>
+        /// <remarks>
+        /// If authentiaction fails an SalesforceException will be throw.
+        /// </remarks>
+        /// <returns>The authentication info with access token and instance url for futher API calls.</returns>
+        Task<AuthenticationInfo> AuthenticateAsync(CancellationToken cancellationToken = default(CancellationToken));
     }
 }
